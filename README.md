cc15a30944f86c4316ac5348493eb20c5da02898
Yuki Koya <ykky@pepabo.com>
2018-05-30 14:02:43 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # live-migration-notifier
 OpenStackのLiveMigrationの通知するくん
+
+## 使い方
+
+```
+$ export RABBITMQ_HOST=<rabbitmq host>
+$ export RABBITMQ_PORT=<rabbitmq port>
+$ export RABBITMQ_USER=<rabbitmq user>
+$ export RABBITMQ_PASSWORD=<rabbitmq password>
+$ export SLACK_WEBHOOK_URL=<webhook url>
+
+$ bundle install
+$ bundle exec ./notifier.rb
+```
+
+systemdで動かす場合には以下のようなUnitファイルを作ります
+
+```
+[Unit]
+Description = live-migration notifier
+
+[Service]
+User=ubuntu
+Group=ubuntu
+Environment = RABBITMQ_HOST=<rabbitmq host>
+Environment = RABBITMQ_PORT=<rabbitmq port>
+Environment = RABBITMQ_USER=<rabbitmq user>
+Environment = RABBITMQ_PASSWORD=<rabbitmq password>
+Environment = SLACK_WEBHOOK_URL=<webhook url>
+ExecStart = /usr/bin/ruby /path/to/notifier.rb
+Restart = always
+Type = simple
+
+[Install]
+WantedBy = multi-user.target
+```
---
+++ notifier.rb
@@ -0,0 +1,146 @@
+#!/usr/bin/env ruby
+
+require 'bunny'
+require 'json'
+require 'slack/incoming/webhooks'
+
+def rabbitmq_init
+  host = ENV['RABBITMQ_HOST']
+  port = ENV['RABBITMQ_PORT'] || 5672
+  user = ENV['RABBITMQ_USER'] || 'guest'
+  pass = ENV['RABBITMQ_PASSWORD'] || 'guest'
+
+  Bunny.new(hostname: host, port: port, user: user, pass: pass)
+end
+
+webhook_url = ENV["SLACK_WEBHOOK_URL"]
+slack = Slack::Incoming::Webhooks.new(webhook_url)
+
+connection = rabbitmq_init
+connection.start
+
+ch = connection.create_channel
+q = ch.queue("live-migration_notifier", exclusive: true)
+x = ch.topic("nova")
+q.bind(x, routing_key: "compute.*")
+
+begin
+  q.subscribe(manual_ack: true, block: true) do |delivery_info, properties, msg|
+    payload = JSON.parse(JSON.parse(msg)["oslo.message"])
+    method = payload["method"]
+
+    case method
+    when "pre_live_migration"
+      instance = payload["args"]["instance"]["nova_object.data"]
+
+      src = instance["host"]
+      dst = delivery_info.routing_key.gsub(/^compute\./, "")
+
+      hostname = instance["display_name"]
+      user = payload["_context_user_name"]
+
+      attachment = [{
+        text: "live-migrationが開始されました",
+        fallback: "start: #{hostname} (#{src} -> #{dst})",
+        color: "warning",
+        fields: [
+          {
+            title: "instance",
+            value: hostname,
+            short: true,
+          },
+          {
+            title: "user",
+            value: user,
+            short: true,
+          },
+          {
+            title: "src",
+            value: src,
+            short: true
+          },
+          {
+            title: "dst",
+            value: dst,
+            short: true
+          },
+        ]
+      }]
+    when "post_live_migration_at_destination"
+      instance = payload["args"]["instance"]["nova_object.data"]
+
+      src = instance["host"]
+      dst = delivery_info.routing_key.gsub(/^compute\./, "")
+
+      hostname = instance["display_name"]
+      user = payload["_context_user_name"]
+
+      attachment = [{
+        text: "live-migrationが完了しました",
+        fallback: "end: #{hostname} (#{dst})",
+        color: "good",
+        fields: [
+          {
+            title: "instance",
+            value: hostname,
+            short: true,
+          },
+          {
+            title: "user",
+            value: user,
+            short: true,
+          },
+          {
+            title: "src",
+            value: src,
+            short: true
+          },
+          {
+            title: "dst",
+            value: dst,
+            short: true
+          },
+        ]
+      }]
+    when "rollback_live_migration_at_destination"
+      instance = payload["args"]["instance"]["nova_object.data"]
+
+      src = instance["host"]
+      dst = delivery_info.routing_key.gsub(/^compute\./, "")
+
+      hostname = instance["display_name"]
+      user = payload["_context_user_name"]
+
+      attachment = [{
+        text: "live-migrationが失敗しました",
+        fallback: "error: #{hostname} (#{dst})",
+        color: "danger",
+        fields: [
+          {
+            title: "instance",
+            value: hostname,
+            short: true,
+          },
+          {
+            title: "user",
+            value: user,
+            short: true,
+          },
+          {
+            title: "src",
+            value: src,
+            short: true
+          },
+          {
+            title: "dst",
+            value: dst,
+            short: true
+          },
+        ]
+      }]
+    end
+    slack.post "", attachments: attachment
+  end
+rescue Interrupt
+  connection.close
+end
